3378e826a2d9ff0af5b3d52760372153ae320c54
draconid719 <draconid719@gmail.com>
2024-01-18 16:45:53 -0800

    fix basepath

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/dayzero/nextjs-github-pages",
+  basePath: "/dayzero",
 
   /**
    * Disable server-based image optimization. Next.js does not support
