ffb8ee8b982157b021d75cef5b4636632962a5e8
draconid719 <draconid719@gmail.com>
2024-01-18 17:08:52 -0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
+  // const router = useRouter();
 
-  router.push("/myroute");
+  // router.push("/myroute");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
